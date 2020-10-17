90e743dc2b4f803dd71208053d3b0f8fc8395f28
Jesse Wozniak <wozniak.jesse@gmail.com>
2020-10-17 11:43:49 -0400

    amp slider range adjustments

--- Source/SympleADSRComponent.cpp
+++ Source/SympleADSRComponent.cpp
@@ -20,6 +20,7 @@ SympleADSRComponent::SympleADSRComponent(SympleSynthAudioProcessor& p) : audioPr
     attack.setSliderStyle (juce::Slider::LinearBarVertical);
     attack.setRange (0.0, 10.0, 0.001);
     attack.setValue (audioProcessor.getAmpParameters().attack);
+    attack.setSkewFactorFromMidPoint(0.35);
     attack.setTextBoxStyle (juce::Slider::TextBoxAbove, false, 90, 30);
     attack.setPopupDisplayEnabled (true, false, this);
     attack.setTextValueSuffix (" sec");
@@ -29,6 +30,7 @@ SympleADSRComponent::SympleADSRComponent(SympleSynthAudioProcessor& p) : audioPr
     decay.setSliderStyle (juce::Slider::LinearBarVertical);
     decay.setRange (0.0, 10.0, 0.001);
     decay.setValue (audioProcessor.getAmpParameters().decay);
+    decay.setSkewFactorFromMidPoint(0.35);
     decay.setTextBoxStyle (juce::Slider::TextBoxAbove, false, 90, 30);
     decay.setPopupDisplayEnabled (false, false, this);
     decay.setTextValueSuffix (" sec");
@@ -36,16 +38,17 @@ SympleADSRComponent::SympleADSRComponent(SympleSynthAudioProcessor& p) : audioPr
     decay.addListener (this);
 
     sustain.setSliderStyle (juce::Slider::LinearBarVertical);
-    sustain.setRange (0.0, 10.0, 0.001);
-    sustain.setValue (audioProcessor.getAmpParameters().sustain);
+    sustain.setRange (0.0, 100.0, 0.01);
+    sustain.setValue (audioProcessor.getAmpParameters().sustain * 100);
     sustain.setTextBoxStyle (juce::Slider::TextBoxAbove, false, 90, 30);
     sustain.setPopupDisplayEnabled (false, false, this);
-    sustain.setTextValueSuffix (" sec");
+    sustain.setTextValueSuffix ("%");
     addAndMakeVisible (&sustain);
     sustain.addListener (this);
 
     release.setSliderStyle (juce::Slider::LinearBarVertical);
     release.setRange (0.0, 10.0, 0.001);
+    release.setSkewFactorFromMidPoint(0.35);
     release.setValue (audioProcessor.getAmpParameters().release);
     release.setTextBoxStyle (juce::Slider::TextBoxAbove, false, 90, 30);
     release.setPopupDisplayEnabled (false, false, this);
@@ -85,7 +88,7 @@ void SympleADSRComponent::sliderValueChanged(juce::Slider* slider)
 
     params.attack = attack.getValue();
     params.decay = decay.getValue();
-    params.sustain = sustain.getValue();
+    params.sustain = sustain.getValue() / 100.0;
     params.release = release.getValue();
     
     audioProcessor.setAmpParameters(params);
